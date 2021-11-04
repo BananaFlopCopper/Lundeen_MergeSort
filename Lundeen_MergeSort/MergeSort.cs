bd31396916eda45f64a2b3bbb4217755a2cd4d05
CopperBanana <aiden.lundeen@student.centralia.edu>
2021-11-03 21:32:26 -0700

    Add project files.

---
+++ Lundeen_MergeSort/MergeSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lundeen_MergeSort
+{
+    class MergeSort
+    {
+        public static void merge(int[] array, int left, int middle, int right)
+        {
+            int loopTime = middle;
+            loopTime++;
+            // +1 to account for offset
+           for (int i = 0; i < loopTime; i++)
+            {
+                int temp;
+                // if right is larger, then swap
+                if(array[i] < array[middle + 1])
+                {
+                    temp = array[i];
+                    array[i] = array[middle + 1];
+                    array[middle + 1] = temp;
+
+                    // sort right hand side, since it's already sorted, we can stop at the first value smaller than this one
+                    for (int j = middle; j < right; j++)
+                    {
+                        if(array[j] < array[j + 1])
+                        {
+                            temp = array[j];
+                            array[j] = array[j+1];
+                            array[j] = temp;
+                        }
+                    }
+                }
+            }
+        }
+       public static int[] Sort(int[] array, int left, int right)
+        { // Largest to Smallest
+            if(right-left > 0)
+            {
+                int middle = (((right-1)+left) / 2);
+                array = Sort(array, left, middle);
+                array = Sort(array, middle + 1, right);
+                merge(array, left, middle, right);
+            }
+            
+
+            
+            return array;
+        }
+    }
+}
---
+++ Lundeen_MergeSort/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lundeen_MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 30, 900, 55, 54, 56, 71, -43, 0, 4529, 555, 639, 101, 102, 100, 5, 82, 50, -77, 14, 99, 104, 1 };
+            array = MergeSort.Sort(array, 0, array.Length-1);
+
+            int screenWidth = 4;
+            for (int i = 0; i < array.Length; i++)
+            {
+                screenWidth--;
+                if(screenWidth == 0)
+                {
+                    screenWidth = 4;
+                    Console.WriteLine(array[i]);
+                } else
+                {
+                    Console.Write(array[i] + ", ");
+                }
+            }
+
+        }
+    }
+}
