645930b296411b7a35cf88c47d93e23d0c61d880
CopperBanana <aiden.lundeen@student.centralia.edu>
2021-11-05 13:44:07 -0700

    Fixed sorting algorithm to not read full list every iteration

--- Lundeen_MergeSort/MergeSort.cs
+++ Lundeen_MergeSort/MergeSort.cs
@@ -10,28 +10,33 @@ namespace Lundeen_MergeSort
     {
         public static void Merge(int[] array, int left, int middle, int right)
         {
-            int loopTime = middle;
-            loopTime++;
+            
             // +1 to account for offset
-           for (int i = 0; i < loopTime; i++)
+           for (int i = left; i <= middle; i++)
             {
                 int temp;
                 // if right 'subarray' is larger, then swap
                 if(array[i] < array[middle + 1])
                 {
+                    //flip
                     temp = array[i];
                     array[i] = array[middle + 1];
                     array[middle + 1] = temp;
 
                     // sort right hand side, since it's already sorted, we can stop at the first value smaller than this one
                     //flops values until value on right is less than current value
-                    for (int j = middle; j < right; j++)
+                    for (int j = middle+1; j < right; j++)
                     {
                         if(array[j] < array[j + 1])
                         {
+                            //flip
                             temp = array[j];
                             array[j] = array[j+1];
-                            array[j] = temp;
+                            array[j+1] = temp;
+                        }
+                        else
+                        {
+                            break;
                         }
                     }
                 }
@@ -41,7 +46,7 @@ namespace Lundeen_MergeSort
         { // Largest to Smallest
             if (right - left > 0)
             {
-                int middle = (((right - 1) + left) / 2);
+                int middle = ((right + left) / 2);
                 Sort(array, left, middle);
                 Sort(array, middle + 1, right);
                 Merge(array, left, middle, right);
